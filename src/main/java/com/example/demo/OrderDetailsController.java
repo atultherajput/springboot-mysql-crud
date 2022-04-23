6659d796c7370e88e3a8d030d3bbddcb85340b79
Atul Krishna <Atul.Krishna@progress.com>
2022-04-24 02:06:27 +0530

    changed mysql driver name.

--- src/main/java/com/example/demo/OrderDetailsController.java
+++ src/main/java/com/example/demo/OrderDetailsController.java
@@ -14,7 +14,7 @@ public class OrderDetailsController {
     public List<Orderdetails> list() throws ClassNotFoundException, SQLException {
         List<Orderdetails> result = new ArrayList<>();
 
-        Class.forName("com.mysql.jdbc.Driver");
+        Class.forName("com.mysql.cj.jdbc.Driver");
         Connection con = DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/classicmodels", "atul", "password");
         Statement stmt = con.createStatement();
